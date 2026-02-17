407a58ddfc9a5378492154fc0a92472f35fd7b36
DVITTALBOTZ <133628140+DVITTALBOTZ@users.noreply.github.com>
2026-02-17 15:11:14 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -136,8 +136,8 @@ class WZMLStyle:
 ------------------------------------------\n\n"""
 
     # async def __msg_to_reply(self): ---> pyrogramEngine.py
-    PM_START = "➲ <b><u>Task Started :</u></b>\n┃\n┖ <b>Link:</b> <a href='{msg_link}'>Click Here</a>"
-    L_LOG_START = "➲ <b><u>Leech Started :</u></b>\n┃\n┠ <b>User :</b> {mention} ( #ID{uid} )\n┖ <b>Source :</b> <a href='{msg_link}'>Click Here</a>"
+    PM_START = "➲ <b><u>🪶Task Started :</u></b>\n┃\n┖ <b>👍Link:</b> <a href='{msg_link}'>Click Here</a>"
+    L_LOG_START = "➲ <b><u>🙌Leech Started :</u></b>\n┃\n┠ <b>📄User :</b> {mention} ( #ID{uid} )\n┖ <b>Source :</b> <a href='{msg_link}'>Click Here</a>"
 
     # async def onUploadComplete(): ---> tasks_listener.py
     NAME = "<b><i>{Name}</i></b>\n┃\n"
